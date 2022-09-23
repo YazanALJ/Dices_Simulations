4ee94cb26948630b76c022460d0d7cf325a41b98
Yazan <91796688+YazanALJ@users.noreply.github.com>
2022-09-23 19:28:38 +0200

    Update 1.0
    Full variable renaming to more comprehensible and explanatory names
    Improved code structure
    Deleted redundancies

--- Simulation.py
+++ Simulation.py
@@ -1,5 +1,5 @@
 from random import randint
-#test here, here too
+
 nb_sims = int(input("Choose the number of simulations "))
 while nb_sims <= 0:
     nb_sims = int(input("Choose a positive number of simulations.. "))
@@ -8,24 +8,20 @@ nb_dice = int(input("Choose the number of dice "))
 while nb_dice <= 0:
     nb_dice = int(input("Choose a positive number of dice.. "))
 
-nbLT = 0
-TTTDS = 0
+nb_of_total_throws = 0
+nb_of_throws = 0
+dice_roll_sum = 0
 for posbl_roll in range(1 * nb_dice, (6 * nb_dice) + 1):
     for sim in range(nb_sims):
-        for NBD in range(1, nb_dice + 1):
-            TTDS = randint(1, 6)
-            TTTDS += TTDS
-        nbL = 0
-        while TTTDS != posbl_roll:
-            TTTDS = 0
+        while dice_roll_sum != posbl_roll:
+            dice_roll_sum = 0
             for NBD in range(1, nb_dice + 1):
-                TTDS = randint(1, 6)
-                TTTDS += TTDS
-            nbL += 1
-
-        nbLT += nbL
-        nbL = 0
-    av_num_trws = nbLT / nb_sims
-    nbLT = 0
-    print("The average number of throws required to have", posbl_roll, "is",
+                dice_roll_sum += randint(1, 6)
+            nb_of_throws += 1
+        nb_of_total_throws += nb_of_throws
+        nb_of_throws = 0
+        dice_roll_sum = 0
+    av_num_trws = nb_of_total_throws / nb_sims
+    nb_of_total_throws = 0
+    print("The average number of throws required to have a score of", posbl_roll, "is",
           av_num_trws)
