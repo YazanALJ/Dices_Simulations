6c4cf376bed629b8989873456a26e6e8efa5eae0
Yazan <91796688+YazanALJ@users.noreply.github.com>
2022-09-23 20:36:35 +0200

    Update 1.1
    Removed redundancies
    Modified the while loop for a better more readable version.

--- Simulation.py
+++ Simulation.py
@@ -10,17 +10,18 @@ while nb_dice <= 0:
 
 nb_of_total_throws = 0
 nb_of_throws = 0
-dice_roll_sum = 0
+
 for posbl_roll in range(1 * nb_dice, (6 * nb_dice) + 1):
     for sim in range(nb_sims):
-        while dice_roll_sum != posbl_roll:
+        while True:
             dice_roll_sum = 0
             for NBD in range(1, nb_dice + 1):
                 dice_roll_sum += randint(1, 6)
             nb_of_throws += 1
+            if dice_roll_sum == posbl_roll:
+                break
         nb_of_total_throws += nb_of_throws
         nb_of_throws = 0
-        dice_roll_sum = 0
     av_num_trws = nb_of_total_throws / nb_sims
     nb_of_total_throws = 0
     print("The average number of throws required to have a score of", posbl_roll, "is",
